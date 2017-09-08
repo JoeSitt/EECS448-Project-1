3e9410c360e6645105fe3a50826e1bbfa39d5421
Carter Maxcy <cartermaxcy@ku.edu>
2017-09-08 17:17:30 -0500

    Created a sample class that can have its contents read/written

--- project-1/src/scheduler/Main.java
+++ project-1/src/scheduler/Main.java
@@ -1,8 +1,25 @@
 package scheduler;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
-		Person p1 = new Person("Carter", 10, "Boy");
-		System.out.println(p1.toString());
+		
+		// Class instantiated with sample member variable
+		List<String> testArr = Arrays.asList(new String[]{"A", "B", "C"});
+		SampleClass s = new SampleClass(testArr);
+		
+		// Class instance written to file
+		s.write("SampleClass-test.txt");
+		
+		// instance read from file and contents verified
+		SampleClass j = SampleClass.read("SampleClass-test.txt");
+		printList(j.getMember());
+	}
+	public static void printList(List<String> list) {
+		for (String item : list) {
+			System.out.println(item);
+		}
 	}
 }
