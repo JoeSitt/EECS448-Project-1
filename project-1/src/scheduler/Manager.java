9f588239853ac21a388df5eb0ae4131804a7f7f6
Carter Maxcy <cartermaxcy@ku.edu>
2017-09-11 14:23:00 -0500

    Created templates for Event and Manager classes

---
+++ project-1/src/scheduler/Manager.java
@@ -0,0 +1,17 @@
+package scheduler;
+
+/**
+ * Keeps track of Events, allowing them to be added and summarized upon request.
+ * 
+ * @author cmaxcy
+ *
+ */
+
+// TODO(cmaxcy):
+// - Implement
+// - Document
+// - Test
+
+public class Manager {
+
+}
