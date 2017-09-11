ed5d05c9df4bbfdf1df54e74e88907c4059ff759
JoeSitt <JoeSitt@live.com>
2017-09-10 19:41:05 -0500

    added the start of user method and tested. it is not finished.

--- project-1/src/scheduler/ui.java
+++ project-1/src/scheduler/ui.java
@@ -24,31 +24,12 @@ public class ui {
 	System.out.println("your name is "+me.uname);
 	System.out.println("Would you rather have a time be displayed in a 24hr format or a 12hr format");
 	System.out.println("type \"12\" for 12hr format and \"24\" for 24hr format ");
-
-	boolean badinput=true;
-
+	//boolean badinput=true;
 	String input;
 	String twelve= new String("12");
-	String twenty4= new String("24");
+	//String twenty4= new String("24");
 	input=myscan.nextLine();
 	String[] goodAnswer= {"12","24"};
-	/*while(badinput == true) {
-		input=myscan.nextLine();
-		System.out.println("input recieved.");
-		
-		if(!(input.equals(twelve))&&!(input.equals(twenty4))) {
-			System.out.println("Your input was not valid. please try again.");
-			System.out.println("Would you rather have a time be displayed in a 24hr format or a 12hr format");
-			System.out.println("type \"12\" for 12hr format and \"24\" for 24hr format ");
-		}else if(input.equals(twelve)){
-			me.timeview=12;
-			badinput=false;
-		}else {
-			me.timeview=24;
-			badinput=false;
-		}
-	}
-	*/	
 	while(!inputcheck(goodAnswer,input)) {
 		input=myscan.nextLine();
 		System.out.println("input recieved.");
@@ -59,13 +40,14 @@ public class ui {
 			System.out.println("type \"12\" for 12hr format and \"24\" for 24hr format ");
 		}else if(input.equals(twelve)){
 			me.timeview=12;
-			badinput=false;
+			//badinput=false;
 		}else {
 			me.timeview=24;
-			badinput=false;
+			//badinput=false;
 		}
 	}
 	clean();
+	user();
 	myscan.close();
 }
 
@@ -86,7 +68,7 @@ public class ui {
 	 * input: a list of good inputs as strings and a string of the users input
 	 * output: a bool of if the users input matched any of the acceptable answers
 	 * modifies: none
-	 * not tested.
+	 * 
 	 */
 	public static boolean inputcheck(String[] goodinputs,String input) {
 		boolean recievedGin=false;
@@ -98,5 +80,19 @@ public class ui {
 		return recievedGin;
 	}
 	
+	/*
+	 * input:none
+	 * output:none
+	 * modifies: screen
+	 * shows menu and acts off of user input to goto view event screens. currently not finished.
+	 */
+	public static void user() {
+		System.out.println("=============================================================================\r\n" + 
+				"Input the index number of the event you want to view or input a code of what you want to do\r\n" + 
+				"Return to options (code: “esc”)\r\n" + 
+				"Create an event (code: “create”)\r\n" + 
+				"=============================================================================\r\n" + 
+				"");
+	}
 
 }
